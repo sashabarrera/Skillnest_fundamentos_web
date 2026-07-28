70ef09376ee645fd7fb03a2ef3071b7c1b8031b2
sashabarrera <sashabarrera@liceovvh.cl>
2026-07-28 15:15:54 -0400

    listo

--- STACK_JS/11_Funciones_JS/static/js/script.js
+++ STACK_JS/11_Funciones_JS/static/js/script.js
@@ -8,7 +8,33 @@ function saludar(/* parametro */) {
 
 saludar(); //Ejecutacion de una funcion
 
+//Funciones con parametros
+//El parametro recibe un valor para trabajar en la funcion.
+//El parametro recibe el tipo de dato al momento de tomar valor.
 
 
+function saludarParam(nombre) { //Parametro nombre
+    alert("¡Hola, " + nombre + "!");
+}
+
+//saludarParam("Sasha"); //Ejecucion de la funcion con parametros. ("argumento")
+saludarParam("Ana");
+
+//Funciones con rectun
+
+function encontrarMayor() {
+    function encontrarMaximo(a, b) {
+        if (a > b) {
+            return a;
+        } else {
+            return b;
+        }
+    }
+    let numero1 = 10;
+    let numero2 = 7;
+    let maximo = encontrarMaximo(numero1, numero2);
+    //Maxino guardara el valor del retorno
+    alert(`El número mayor entre ${numero1}, y ${numero2}, es: ${maximo}`);
+}
 
 
