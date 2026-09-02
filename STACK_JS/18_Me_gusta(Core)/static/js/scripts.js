ba830cb0f5dac2770c15b8176ee7c22187b9c681
sashabarrera <sashabarrera@liceovvh.cl>
2026-09-02 15:56:45 -0400

    OK

--- STACK_JS/18_Me_gusta(Core)/static/js/scripts.js
+++ STACK_JS/18_Me_gusta(Core)/static/js/scripts.js
@@ -1,1 +1,21 @@
-console.log("Conexion correcta...");
+console.log("Conexión correcta...");
+
+let publicaciones = document.querySelectorAll('.contenedor-likes');
+
+for (let i = 0; i < publicaciones.length; i++) {
+    let publicacion = publicaciones[i];
+
+
+    let boton = publicacion.querySelector('.descripcion-boton');
+    let contador = publicacion.querySelector('#meGustas');
+    let likes = 0;
+
+    if (boton && contador) {
+        boton.addEventListener('click', () => {
+            likes++;
+            contador.textContent = `${likes} like(s)`;
+        });
+    }
+}
+
+
